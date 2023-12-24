5f24a885d73060818258e7f666b7e935bb3b09a5
Jeffery <jefferymutuku283@gmail.com>
2023-12-24 07:16:04 -0800

    Task 3

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * integer_swap- This swaps two integers
+ * @i: This is the first
+ * @j: This is the second
+ */
+
+void integer_swap(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/**
+ * selection_sort - This sorts an array of integers
+ * @array: This is an array of integers
+ * @ size: This is the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int *minimum;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++ )
+	{
+		minimum = array + i;
+		for (j = i + 1; j < size; j++)
+			minimum = (array[j] < *minimum) ? (array + j) : minimum;
+
+		if ((array + i) != minimum)
+		{
+			integer_swap(array + i, minimum);
+			print_array(array, size);
+		}
+	}
+}
